d3c2409638a6150c529aa97ad89b40fdebe8b0a7
jac2206 <arango773@gmail.com>
2026-09-25 18:00:45 -0500

    fix: delete comment

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,19 +7,6 @@ export class UserController {
         readonly userService: IUserInterface
     ) {}
 
-    // async createUser(req:Request, res:Response):Promise<void>{
-    //     const request: UserRequestDTO = req.body as UserRequestDTO 
-    //     if (!request.name) {
-    //         res.status(400).json({
-    //             code: "BAD_REQUEST",
-    //             message: "name not exist"
-    //         });
-    //     };
-
-    //     const result = await this.userService.createUser(request);
-    //     res.status(200).json(result);       
-    // }
-
     createUser = async (
         req: Request,
         res: Response,
